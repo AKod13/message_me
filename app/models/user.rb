ee737299303c3c1d19019221aff37920f75363c8
Akod <akodzoman13@gmail.com>
2023-06-08 11:40:25 +0200

    Remake of the index view and development of messages partial

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
   validates :username, uniqueness: { case_sensitive: false }, presence: true, length: { minimum: 3, maximum: 30 }
   has_many :messages
-  #has_secure_password
+  has_secure_password
 end
