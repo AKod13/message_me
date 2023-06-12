b4668fe4c809c72f482a588e8e41861234656d21
Akod <akodzoman13@gmail.com>
2023-06-12 12:43:58 +0200

    Added flash messages for login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,8 +3,20 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = User.find_by(username: params[:session][:username])
+    if user && user.authenticate(params[:session][:password])
+      session[:user_id] = user.id
+      flash[:success] = "You have succesfully logged in"
+      redirect_to root_path
+    else
+      flash.now[:error] = "There was something wron with your login information! "
+      render 'new'
+    end
   end
 
   def destroy
+    session[:user_id] = nil
+    flash[:notice] = " You have succesfuly logged out"
+    redirect_to root_path
   end
 end
